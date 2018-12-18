59a9a608ba6d5756e75976d79a4393db3b4c0b15
五十嵐太郎 <igarashitaro@MacBookmujirushi.local>
2018-12-19 05:15:57 +0900

    7th commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,18 @@ class User < ApplicationRecord
   validates :user_name, presence: true, length: { maximum: 50 }
 
   # allow users to update their accounts without passwords
-  def update_without_current_password
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+    
+  end
 
 end
